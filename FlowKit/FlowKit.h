7d548d9692cadd29b681a24fe73ed7e4493bd71e
Xiaodi Wu <xiaodi.wu@gmail.com>
2016-06-18 12:43:27 -0500

    Initial Commit

---
+++ FlowKit/FlowKit.h
@@ -0,0 +1,19 @@
+//
+//  FlowKit.h
+//  FlowKit
+//
+//  Created by Xiaodi Wu on 6/18/16.
+//  Copyright © 2016 Xiaodi Wu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlowKit.
+FOUNDATION_EXPORT double FlowKitVersionNumber;
+
+//! Project version string for FlowKit.
+FOUNDATION_EXPORT const unsigned char FlowKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowKit/PublicHeader.h>
+
+
