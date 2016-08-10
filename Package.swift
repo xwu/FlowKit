27eb4a5ff8d88746ca492f072d37fc51e2b9a709
Xiaodi Wu <xiaodi.wu@gmail.com>
2016-08-09 20:00:54 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+//
+//  Package.swift
+//  FlowKit
+//
+//  Created by Xiaodi Wu on 8/9/16.
+//  Copyright © 2016 Xiaodi Wu. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "FlowKit",
+  exclude: [
+    "Tests/Resources"
+  ]
+)
